6f422a48576c88e46c1f4d688109538f04a9dc56
edjo93 <eddas.carrasco@unitec.edu>
2019-05-16 18:25:04 -0600

    se agrego prototipo de funcion y definicion de funcion bajo el nombre primo

--- lab.cpp
+++ lab.cpp
@@ -3,9 +3,21 @@ using std::cout;
 using std::endl;
 using std::cin;
 
+bool es primo(int);//prototipo de funcion
+
 int main(){
-    
+	//    
     
     
 }
 
+//definicion de funcion
+
+//la funcion es primo devuelve verdadero cuando valida un numero primo
+bool es primo(int num){
+    int counter=0;
+    //un numero es primo cuando solo se se puede dividir entre 1 y el mismo
+    
+    
+    
+}
