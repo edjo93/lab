b76e5adba9bb5e6144337c3e1ac5d3c89e97861e
edjo93 <eddas.carrasco@unitec.edu>
2019-05-16 18:36:00 -0600

    se termino la funcion es primo

--- lab.cpp
+++ lab.cpp
@@ -15,9 +15,16 @@ int main(){
 
 //la funcion es primo devuelve verdadero cuando valida un numero primo
 bool es primo(int num){
-    int counter=0;
-    //un numero es primo cuando solo se se puede dividir entre 1 y el mismo
-    
-    
-    
+  	 
+       	int counter=0;
+  
+	//un numero es primo cuando solo se se puede dividir entre 1 y el mismo
+	 		 	 		 
+    	for(int i=1;i<=num;i++){//prueba exhaustiva de numeros entre [1 y num] 
+		if(num%i==0){
+			counter++;
+		}
+	}
+    	
+	return counter==2;//si el counter es 2 significa que el numero es divisible solamente entre 1 y el mismo
 }
