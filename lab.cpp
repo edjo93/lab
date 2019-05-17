477881fbf2687febb394faa98b525e3a75f04a6b
edjo93 <eddas.carrasco@unitec.edu>
2019-05-16 18:39:11 -0600

    menu repetitivo agregado

--- lab.cpp
+++ lab.cpp
@@ -6,7 +6,26 @@ using std::cin;
 bool es primo(int);//prototipo de funcion
 
 int main(){
-	//    
+	// menu repetitivo
+	char opcion;
+        do{
+                cout<<"\ningrese una opcion\n1.op1\n2.op2\n3.op3\n4.salir\n\n?:";cin>>opcion;
+                //la opcion es procesada en una estructura de casos
+                switch(opcion){
+                        case '1':
+                                break;
+                        case '2':
+                                break;
+
+                        case '3': 
+                                break;
+                        case '4':
+                                cout<<"\nhasta luego"<<endl;
+                                break;
+
+                        default:cout<<"\nno ingreso una opcion correcta/intente de nuevo"<<endl;
+                }
+        }while(opcion!='4');   
     
     
 }
@@ -27,4 +46,6 @@ bool es primo(int num){
 	}
     	
 	return counter==2;//si el counter es 2 significa que el numero es divisible solamente entre 1 y el mismo
+
 }
+
